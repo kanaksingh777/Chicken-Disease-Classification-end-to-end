b9666f69cae3a323ca0e71cad6d31604dcc5413d
kanaksingh777 <kanaksingh2785@gmail.com>
2024-10-04 18:33:33 +0100

    Your commit message

--- template.py
+++ template.py
@@ -4,9 +4,47 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format = '[%(asctime)s]:%(message)s:')
 
-project_name = "cnn_Classifier"
+
+project_name = "cnnClassifier"
 
 list_of_files = [
-    ".github/workflows/.gitkeep"
-                 ]
-#checking for the git commit 
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    #now the filepath has both the things that is file path and filename 
+    filedir, filename = os.path.split(filepath)  
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok =True) #checks if the directory is already created or not 
+        logging.info(f"Creating Directory ; {filedir} for the file:{filename}")
+
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+           #checking if the file exists or not , checking the size of the file 
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Creating empty file :{filepath}")
+
+    else:
+        logging.nfo(f"{filename} already exists")
+
+
